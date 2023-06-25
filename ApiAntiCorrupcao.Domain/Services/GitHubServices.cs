f1d3742b9898d648fe54144e032ef28671110f1d
Leonardo Nascimento <leonardot.i@hotmail.com>
2023-06-25 13:35:25 -0300

    iniciando o projeto

---
+++ ApiAntiCorrupcao.Domain/Services/GitHubServices.cs
@@ -0,0 +1,14 @@
+﻿using ApiAntiCorrupcao.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiAntiCorrupcao.Domain.Services
+{
+    public class GitHubServices : IGitHubServices
+    {
+
+    }
+}
